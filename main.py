3c25f7389264242e0eefb11df4d802e5993aff5d
TepidMilk <ivorykingrey@gmail.com>
2025-09-16 15:08:05 -0500

    Update main to generate a response from args

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from custom-ai-agent!")
+import os, sys, argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user_prompt')
+    parser.add_argument('-v', '--verbose', action='store_true', help="show token usage and response roles")
+    args = parser.parse_args()
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+    p_tokens = response.usage_metadata.prompt_token_count
+    r_tokens = response.usage_metadata.candidates_token_count
+    if args.verbose:     
+        print(f'User prompt: {args.user_prompt}\n')
+        print(f'Prompt tokens: {p_tokens}')
+        print(f'Response tokens: {r_tokens}\n')
+    print(response.text)
 
 if __name__ == "__main__":
     main()
