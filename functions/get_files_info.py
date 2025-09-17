03339e62e74bb83072fb618aee132f72d9c16ca3
TepidMilk <ivorykingrey@gmail.com>
2025-09-17 15:09:09 -0500

    Update get_files_info
    was marking every directory as outside of the
    working directory. Now correctly identifies
    working directory limits

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,13 +3,13 @@ import os
 
 def get_files_info(working_directory, directory="."):
     res = []
-    full_directory = os.path.join(working_directory, directory)
-    path = os.path.abspath(full_directory)
+    abs_working = os.path.abspath(working_directory)
+    path = os.path.abspath(os.path.join(working_directory, directory))
 
     # check if the given path is relative to the working_directory so the AI agent doesn't work outside our working_directory
     if not os.path.exists(path):
         return f'Error: path to "{directory}" does not exist'
-    if not path.startswith(working_directory):
+    if not path.startswith(abs_working):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(path):
         return f'Error: "{directory}" is not a directory'
